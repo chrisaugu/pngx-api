e292bb539611f20784bf64493db5c4b46357a9bb
Christian Augustyn <30349879+chrisaugu@users.noreply.github.com>
2021-10-02 16:50:07 +1000

    fix date issue

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const PORT = process.env.PORT;
 app.set('port', PORT);
 app.set('env', process.env.ENV);
 
-// api
+// /v1
 app.use('/v1', router);
 
 // create server and listen on the port
@@ -40,12 +40,8 @@ mongoose.connection.on('error', function(){
 // models
 const Schema = mongoose.Schema;
 
-function convertDate(e) {
-	return e;
-}
-
 var quoteSchema = new Schema({
-	date: {type: Date, default: Date.now, index: true, get: convertDate},
+	date: {type: Date},
 	code: String,
 	short_name: String,
 	bid: Number,
@@ -63,9 +59,9 @@ var quoteSchema = new Schema({
 const Stock = mongoose.model('stockquote', quoteSchema);
 
 // var kitty = new Stock({
-// 	date: "2021-09-01",
-// 	code: "BSP",
-// 	short_name: "BSP",
+// 	date: "2021-09-02",
+// 	code: "CPL",
+// 	short_name: "CPL",
 // 	bid: 0,
 // 	offer: 12.6,
 // 	last: 12.6,
@@ -133,68 +129,70 @@ cron.schedule('* 5 * * *', function() {
 	console.log('running a task every 5 minutes');
 
 	console.log('Fetching data from https://www.pngx.com.pg');
-	get_quotes_from_pngx(DATAURL);
+	for (var i = 0; i < QUOTES.length; i++) {
+
+		get_quotes_from_pngx(DATAURL, QUOTES[i]).then(function(response) {
+			var totalCount = response.length, totalAdded = 0;
+			console.log("start");
+			console.log("Adding quotes for " + QUOTES[i] + " ...");
+
+			response.forEach(function(data) {
+				// check if the quote for that particular company at that particular date already exists
+				Stock.findOne({
+					'date': data['Date'],
+					'short_name': data['Short Name']
+				})
+				.lean()
+				.exec()
+				.then(function(result) {
+					if (result == null) {
+						let quote = new Stock();
+
+						quote['date'] = new Date(data['Date']);
+						quote['code'] = data['Short Name'];
+						quote['short_name'] = data['Short Name'];
+						quote['bid'] = Number(data['Bid']);
+						quote['offer'] = Number(data['Offer']);
+						quote['last'] = Number(data['Last']);
+						quote['close'] = Number(data['Close']);
+						quote['high'] = Number(data['High']);
+						quote['low'] = Number(data['Low']);
+						quote['open'] = Number(data['Open']);
+						quote['chg_today'] = Number(data['Chg. Today']);
+						quote['vol_today'] = Number(data['Vol. Today']);
+						quote['num_trades'] = Number(data['Num. Trades']);
+
+						quote.save(function(err) {
+							if (err) {
+								console.log(err);
+							} else {
+								console.log('added quote for ' + data['Date']);
+								totalAdded++;
+							}
+						});
+					}
+				});
+			});
+
+			console.log(totalAdded + "/" + totalCount + " quotes were added.");
+			console.log("stop");
+		})
+		.catch(function(error) {
+			console.log(error);
+		});
+
+	}
 	console.log('Data fetched from https://www.pngx.com.pg');
 });
+
 // Remove the error.log file every twenty-first day of the month.
-cron.schedule('0 0 21 * *', function() {
-	console.log('---------------------');
-	console.log('Running Cron Job');
-	fs.unlink('./error.log', err => {
-		if (err) throw err;
-		console.log('Error file successfully deleted');
-	});
-});
-
-// get_quotes_from_pngx(DATAURL, QUOTES[0]).then(function(response) {
-// 	console.log("start")
-// 	response.forEach(function(data) {
-// 		// check if the quote for that particular company at that particular date already exists
-// 		Stock.findOne({
-// 			'date': data['Date'],
-// 			'short_name': data['Short Name']
-// 		})
-// 		.lean()
-// 		.exec()
-// 		.then(function(result) {
-// 			if (result == null) {
-// 				let quote = new Stock();
-
-// 				quote['date'] = data['Date'];
-// 				quote['code'] = data['Short Name'];
-// 				quote['short_name'] = data['Short Name'];
-// 				quote['bid'] = data['Bid'];
-// 				quote['offer'] = data['Offer'];
-// 				quote['last'] = data['Last'];
-// 				quote['close'] = data['Close'];
-// 				quote['high'] = data['High'];
-// 				quote['low'] = data['Low'];
-// 				quote['open'] = data['Open'];
-// 				quote['chg_today'] = data['Chg. Today'];
-// 				quote['vol_today'] = data['Vol. Today'];
-// 				quote['num_trades'] = data['Num. Trades'];
-
-// 				quote.save(function(err) {
-// 					if (err) {
-// 						console.log(err);
-// 					} else {
-// 						console.log('saved');
-// 					}
-// 				});
-
-// 			}
-// 		});
+// cron.schedule('0 0 21 * *', function() {
+// 	console.log('---------------------');
+// 	console.log('Running Cron Job');
+// 	fs.unlink('./error.log', err => {
+// 		if (err) throw err;
+// 		console.log('Error file successfully deleted');
 // 	});
-// 	console.log("end")
-// })
-// .catch(function(error) {
-// 	console.log(error);
-// });
-
-// const schedule = require('node-schedule');
-
-// const job = schedule.scheduleJob('42 * * * *', function(){
-//   console.log('The answer to life, the universe, and everything!');
 // });
 
 app.get('/', function(req, res) {
@@ -225,10 +223,11 @@ router.get('/', function(req, res) {
  * @param: /pnx?code=CODE&from=DATE&to=DATE
  */
 router.get('/stocks', function(req, res, next) {
-	// let code = req.query.code || '';
-	let date = req.query.date || '';
-	let start = req.query.start || '';
-	let end = req.query.end || '';
+	let date = req.query.date;
+	let start = req.query.start;
+	let end = req.query.end;
+	let limit = req.query.limit;
+	let sort = req.query.sort;
 
 	// let date = new Date(req.query.date);
 
@@ -280,20 +279,42 @@ router.get('/stocks', function(req, res, next) {
 
 	let stock = Stock.find({});
 
-	// if (code) {
-	// 	stock.where('code', `/${code}/i`);
-	// }
 	if (date) {
-		stock.where('date', new Date(date));
-	}
-	if (start) {
-		stock.where('date').gte(new Date(start));
-	}
-	if (end) {
-		stock.where('date').lte(new Date(end));
+		if (Number.isInteger(Number(date))) {
+			stock.where({ date: date });
+		}
+		else {
+			stock.where({ date: new Date(date) });
+		}
 	}
 
-	stock.limit(100)
+	if (start) {
+		if (Number.isInteger(Number(date))) {
+			stock.where({ date: { $gte: start } });
+		}
+		else {
+			stock.where({ date: { $gte: new Date(start) } });
+		}
+	}
+
+	if (end) {
+		if (Number.isInteger(Number(date))) {
+			stock.where({ date: { $lte: end } });
+		}
+		else {
+			stock.where({ date: { $lte: new Date(end) } });
+		}
+	}
+
+	if (sort) {
+		stock.sort({ date: 1 });
+	} else {
+		stock.sort({ date: 1 });
+	}
+
+	if (limit) {
+		stock.limit(100);
+	}
 
 	stock.exec(function(err, stocks) {
 		if (err) {
@@ -309,6 +330,7 @@ router.get('/stocks', function(req, res, next) {
 			});
 		}
 	});
+
 });
 
 /**
@@ -317,7 +339,6 @@ router.get('/stocks', function(req, res, next) {
  */
 router.get('/stocks/:symbol', function(req, res, next) {
 	let symbol = req.params.symbol
-	let code = req.query.code || '';
 	let date = req.query.date || '';
 	let start = req.query.start || '';
 	let end = req.query.end || '';
@@ -326,20 +347,26 @@ router.get('/stocks/:symbol', function(req, res, next) {
 	stock.where({ code: symbol })
 	// /^vonderful/i)
 
-	if (code) {
-		stock.where('code', `/${code}/i`);
-	}
 	if (date) {
 		stock.where('date', new Date(date));
 	}
 	if (start) {
-		stock.where('date').gte(new Date(start));
+		stock.where({ date: { $gte: new Date(start) } });
 	}
 	if (end) {
-		stock.where('date').lte(new Date(end));
+		stock.where({ date: { $lte: new Date(end) } })
 	}
 
-	stock.limit(100)
+	if (sort) {
+		stock.sort({ date: 1 });
+	} else {
+		stock.sort({ date: 1 });
+	}
+
+	if (limit) {
+		stock.limit(100);
+	}
+
 	stock.exec(function(err, stocks){
 		if (err) {
 			console.log(err);
