71849cc645ac6fd5d2bd7e65cadab6c5848d8446
Christian Augustyn <chrisaugu61@gmail.com>
2025-04-04 18:47:56 +1000

    added websocket, sse, webhook, completed package lib,

--- app.js
+++ app.js
@@ -110,7 +110,7 @@ initDatabase()
     // console.log('This script will run every 2 minutes to update stocks info.');
     // cron.schedule("*/2 * * * *", () => {
     console.log("Stocks info will be updated every morning at 30 minutes past 8 o'clock");
-    // cron.schedule("30 8 * * *", () => {
+    cron.schedule("30 8 * * *", () => {
       // tasks.data_fetcher();
       // const fetch_data_from_pngx = celeryClient.createTask("tasks.fetch_data_from_pngx")
       // 								 .applyAsync(["https://www.pngx.com.pg/data/BSP.csv"]);
@@ -152,7 +152,7 @@ initDatabase()
       } catch (erorr) {
         console.log(erorr);
       }
-    // });
+    });
   })
   .on("error", function () {
     console.log("[Main_Thread]: Error: Could not connect to MongoDB. Did you forget to run 'mongod'?");
