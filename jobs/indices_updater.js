87868b827f82fc2543239a47077db8c253ce89c2
Christian Augustyn <chrisaugu61@gmail.com>
2025-10-19 22:00:19 +1000

    added indices worker

---
+++ jobs/indices_updater.js
@@ -0,0 +1,52 @@
+let up = {
+  code: "PNGXD",
+  name: "PNGXD",
+  components: [
+    {
+      stockSymbol: "BSP",
+      weight: 65.34,
+    },
+    {
+      stockSymbol: "CPL",
+      weight: 4.54,
+    },
+    {
+      stockSymbol: "COY",
+      weight: 2.45,
+    },
+    {
+      stockSymbol: "CCP",
+      weight: 7.26,
+    },
+    {
+      stockSymbol: "KAM",
+      weight: 2.54,
+    },
+    {
+      stockSymbol: "KSL",
+      weight: 5.44,
+    },
+    {
+      stockSymbol: "NGP",
+      weight: 0.91,
+    },
+    {
+      stockSymbol: "CGA",
+      weight: 1.36,
+    },
+    {
+      stockSymbol: "NIU",
+      weight: 0.36,
+    },
+    {
+      stockSymbol: "SST",
+      weight: 9.8,
+    },
+  ],
+  exchange: "PNGX",
+  currency: "PGK",
+  isActive: true,
+  lastUpdated: "2025-07-14T12:51:09.773Z",
+  history: [],
+  percentChange: null,
+};
